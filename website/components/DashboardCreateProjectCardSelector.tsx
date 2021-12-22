2857e633c5ead203925495eb745f8c2ec92ead09
Esteban Garcia <estebangarcia2121@gmail.com>
2021-12-21 16:48:38 -0800

    Added framework logos and create-project css module

--- website/components/DashboardCreateProjectCardSelector.tsx
+++ website/components/DashboardCreateProjectCardSelector.tsx
@@ -17,15 +17,18 @@ export function DashboardCreateProjectCardSelector({
   name
 }: DashboardCreateProjectCardSelectorProps) {
   return (
-    <div className="p-6" onClick={() => setSelected && setSelected(!selected)}>
+    <div
+      className="p-10 flex-1 border rounded cursor-pointer hover:shadow transition"
+      onClick={() => setSelected && setSelected(!selected)}
+    >
       <Image
         src={imageSrc}
         alt={imageAlt}
-        {...responsiveImageProps({ height: '100%', width: '100%' })}
+        {...responsiveImageProps({ height: '10%' })}
       />
 
-      <div className="text-center">
-        <h1 className="text-2xl font-bold">{name}</h1>
+      <div className="text-center mt-8">
+        <h1 className="text-2xl font-medium">{name}</h1>
       </div>
     </div>
   );
--- website/pages/dashboard/create-project.tsx
+++ website/pages/dashboard/create-project.tsx
@@ -1,14 +1,45 @@
+import styles from 'styles/dashboard/create-project.module.css';
+import { DashboardCreateProjectCardSelector } from 'components/DashboardCreateProjectCardSelector';
 import { ContentContainer } from 'components/misc/ContentContainer';
+import { useForm } from 'react-hook-form';
+
+interface FormProps {
+  projectName: string;
+  createGithubRepo: boolean;
+}
 
 export default function CreateProject() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <div>
       <title>Create Project</title>
 
       <ContentContainer tag="main" className="my-32">
-        <h1 className="text-4xl font-semibold">
-          What would you like to build today?
-        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <section className={styles.section}>
+            <h1>What technologies will you be using in your app?</h1>
+          </section>
+
+          <div className="flex flex-row mt-8 space-x-10">
+            <DashboardCreateProjectCardSelector
+              name="Express"
+              imageSrc="/images/frameworks/rest-api.png"
+            />
+            <DashboardCreateProjectCardSelector
+              name="NextJS"
+              imageSrc="/images/frameworks/nextjs-circle.svg"
+            />
+          </div>
+
+          <button type="submit">Create project</button>
+        </form>
       </ContentContainer>
     </div>
   );
