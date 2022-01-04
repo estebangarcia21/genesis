51cbe6d5b117fc3ab32e75c20903a7019f0d450c
Esteban Garcia <estebangarcia2121@gmail.com>
2022-01-03 19:35:13 -0800

    Changed production generator wasm module path

--- api/src/wasm.ts
+++ api/src/wasm.ts
@@ -3,5 +3,5 @@ export let genesisGenerator: typeof import('../../generator/pkg/genesis_generato
 if (process.env.NODE_ENV === 'development' || process.env.IS_OFFLINE) {
   genesisGenerator = require('../../generator/pkg/genesis_generator');
 } else {
-  genesisGenerator = require('./wasm_pkg/genesis_generator');
+  genesisGenerator = require('./artifacts/wasm_pkg/genesis_generator');
 }
